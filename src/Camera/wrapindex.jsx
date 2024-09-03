c4befa1b54e0b63adceb71b2de0b1116af8f5ae0
Geoff <geoff.christy@logieagle.com>
2024-09-03 12:04:31 +0530

    new

---
+++ src/Camera/wrapindex.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { render } from "react-dom";
+import CameraWrap from "./CameraWrap";
+
+// // Render at the end of the body of any website
+// render(<CameraWrap />, window.document.querySelector("#app-container"));
+
+// if (module.hot) module.hot.accept();
