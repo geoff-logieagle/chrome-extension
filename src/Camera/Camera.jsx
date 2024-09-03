e0e9e62af261b17a7e61d8d82e638a37b6cae251
Geoff <geoff.christy@logieagle.com>
2024-09-03 11:51:12 +0530

    new

--- src/Camera/Camera.jsx
+++ src/Camera/Camera.jsx
@@ -1,5 +1,12 @@
-function Camera() {
 
+function Camera() {
+    return (
+        <>
+            <iframe
+                src={chrome.runtime.getURL("camera.html")}
+            >
+            </iframe>
+        </>)
 }
 
 export default Camera;
